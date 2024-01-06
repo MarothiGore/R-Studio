30c6874c37c4188eb97b23e8e97e0982e7275e2e
MarothiGore <119786992+MarothiGore@users.noreply.github.com>
2024-01-06 19:09:12 +0530

    Add files via upload

---
+++ Data_types.R
@@ -0,0 +1,59 @@
+
+#Types of Data in R programming
+
+10+4654
+a<-10+646164
+
+
+rm(a)     #Removing  Data permanently for memory.a/2
+a=567889
+a/2
+A10 <-1200 #Variable Can start with Letter  
+rm(A10)
+B10<-"String"
+rm(B10)
+print(B10)
+N10<-'String'
+print(N10)
+
+A10<-2344
+X<-45
+rm(X)
+X
+rm(x)
+
+x<-45
+X
+rm(X)
+
+l<-TRUE #Boolean Type of Data
+class(l)#Used for Identify the Type of Data in R
+s<-FALSE
+###########################################
+
+t<-c(23,56,78,8,67)
+class(t)
+t1<-c('a','t','j')
+t2<-c('a',34.5,TRUE,'HI')
+
+t1[1]
+t1
+t[1:5]
+t[4]
+t2
+t[3]
+t2
+
+
+
+
+#Acesssing elements form Vector
+
+t<-c(23,56,78,8,67)
+class(t)
+t1<-c('a','t','j')
+t2<-c('a',34.5,TRUE,'HI')
+t[c(1,5)]
+t
+#List Creation
+#Data  Frame Creation
